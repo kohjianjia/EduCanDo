467a24f2c7aa2ded97bf0a74404cb4a9d19165a1
os0022 <oaaos@hotmail.com>
2018-07-25 11:16:16 +0800

    done profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < Clearance::UsersController
 		end
 	end
 
+	def show
+		@user = User.find(params[:id])
+	end
+
 	private
 	def user_params
 	    params.require(:user).permit(:email, :password, :first_name, :last_name, :role)
