88ac443799eb2cd6a04766114a33b22c28bc18e5
os0022 <oaaos@hotmail.com>
2018-08-02 12:11:48 +0800

    faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+user = {}
+user['password'] = '123'
+
+ActiveRecord::Base.transaction do
+  20.times do 
+    user['first_name'] = Faker::Name.first_name 
+    user['last_name'] = Faker::Name.last_name
+    user['email'] = Faker::Internet.email
+    user['role'] = ["student","staff"].sample  
+    user['point'] = [0,500,1700,5050,7500].sample
+
+    User.create(user)
+  end
+end 
+
+# Seed Listings
+event = {}
+uids = []
+User.all.each { |u| uids << u.id }
+
+ActiveRecord::Base.transaction do
+  20.times do 
+    event['title'] = Faker::App.name
+    event['category'] = ["class", "presentation", "seminar", "sport", "other"].sample
+
+    event['description'] = Faker::Hipster.sentence
+    event['venue'] = Faker::University.name
+
+    event['event_date'] = Faker::Date.forward(60)
+    event['start_time'] = ["08:00","08:10", "08:20", "08:30", "08:40"].sample
+    event['end_time'] = ["09:40","09:50", "10:00", "10:10", "10:30"].sample
+     
+
+
+    event['user_id'] = uids.sample
+
+    Event.create(event)
+  end
+end
